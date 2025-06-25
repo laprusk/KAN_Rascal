c64874535dbde5e75a4e4c05eb8489a1b7ff757e
laprusk <149079151+laprusk@users.noreply.github.com>
2025-06-25 17:00:58 +0900

    変数設定

--- kan.c
+++ kan.c
@@ -1,2 +1,5 @@
 #include "kan.h"
 
+double Out[L][NMAX];
+double Coeff[L][NMAX][NMAX][G + SPLINE_K];
+double Wb[L][NMAX][NMAX], Ws[L][NMAX][NMAX];
--- kan.h
+++ kan.h
@@ -1,2 +1,6 @@
 #pragma once
 
+#define L 3
+#define NMAX 5
+#define G 5
+#define SPLINE_K 3
